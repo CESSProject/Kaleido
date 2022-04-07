7fad27d537f6a989dba19f2d7237dddb87aa3f31
Teh Sunn Liu <tehsunnliu@gmail.com>
2022-04-07 13:26:39 +0530

    Added RNG of fixed size and script to start aesm inside docker

--- enclave/src/lib.rs
+++ enclave/src/lib.rs
@@ -17,48 +17,30 @@
 
 #![crate_name = "helloworldsampleenclave"]
 #![crate_type = "staticlib"]
-
 #![cfg_attr(not(target_env = "sgx"), no_std)]
 #![cfg_attr(target_env = "sgx", feature(rustc_private))]
 
+extern crate sgx_rand;
 extern crate sgx_types;
+
 #[cfg(not(target_env = "sgx"))]
 #[macro_use]
 extern crate sgx_tstd as std;
 
+use core::convert::TryInto;
+
+use sgx_rand::{Rng, StdRng};
 use sgx_types::*;
-use std::string::String;
-use std::vec::Vec;
-use std::io::{self, Write};
-use std::slice;
 
 #[no_mangle]
-pub extern "C" fn say_something(some_string: *const u8, some_len: usize) -> sgx_status_t {
-
-    let str_slice = unsafe { slice::from_raw_parts(some_string, some_len) };
-    let _ = io::stdout().write(str_slice);
-
-    // A sample &'static string
-    let rust_raw_string = "This is a in-Enclave ";
-    // An array
-    let word:[u8;4] = [82, 117, 115, 116];
-    // An vector
-    let word_vec:Vec<u8> = vec![32, 115, 116, 114, 105, 110, 103, 33];
-
-    // Construct a string from &'static string
-    let mut hello_string = String::from(rust_raw_string);
-
-    // Iterate on word array
-    for c in word.iter() {
-        hello_string.push(*c as char);
-    }
-
-    // Rust style convertion
-    hello_string += String::from_utf8(word_vec).expect("Invalid UTF-8")
-                                               .as_str();
-
-    // Ocall to normal world for output
-    println!("{}", &hello_string);
+pub extern "C" fn get_rng(value: &mut [u8; 32]) -> sgx_status_t {
+    let mut rng = match StdRng::new() {
+        Ok(rng) => rng,
+        Err(_) => {
+            return sgx_status_t::SGX_ERROR_UNEXPECTED;
+        }
+    };
+    rng.fill_bytes(value);
 
     sgx_status_t::SGX_SUCCESS
 }
