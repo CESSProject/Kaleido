c0e5f6fb6b8e5c4b8a6d0c8843eb2b1cee4a1072
Teh Sunn Liu <tehsunnliu@gmail.com>
2022-04-12 17:59:41 +0530

    Added commands to install PBC and its dependencies to install.sh script

--- install.sh
+++ install.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
-apt update && apt install -y --no-install-recommends \
-                bison \
-                flex \
-                libgmp3-dev \
-                clang \
-    && rm -rf /var/lib/apt/lists/*
+cd ..
+git clone https://github.com/blynn/pbc
+cd pbc
+apt update -y && \
+    apt upgrade -y && \
+    apt dist-upgrade -y && \
+    apt install build-essential software-properties-common -y && \
+    add-apt-repository ppa:ubuntu-toolchain-r/test -y && \
+    apt update -y && \
+    apt install gcc-9 g++-9 -y && \
+    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9 && \
+    update-alternatives --config gcc
 
-export PBC_VERSION=0.5.14
-
-set -ex \
-        \
-        && wget -O pbc.tar.gz "https://crypto.stanford.edu/pbc/files/pbc-$PBC_VERSION.tar.gz" \
-        && mkdir -p /usr/src/pbc \
-        && tar -xzC /usr/src/pbc --strip-components=1 -f pbc.tar.gz \
-        && rm pbc.tar.gz \
-	    \
-	    && cd /usr/src/pbc \
-        && ./configure \
-        && make \
-        && make install \
-        && rm -rf /usr/src/pbc \
-        && cd ~/Kaleido \
-        && make
+apt install -y autoconf \
+        automake \
+        libtool \
+        bison \
+        flex \
+        libgmp3-dev \
+        clang \
+    && rm -rf /var/lib/apt/lists/* \
+    && autoreconf --verbose --install --force \
+    && ./configure \
+    && make \
+    && make install \
+    && cd .. \
+    && rm -rf pbc
 
 LD_LIBRARY_PATH=/opt/intel/sgx-aesm-service/aesm/
 /opt/intel/sgx-aesm-service/aesm/aesm_service
