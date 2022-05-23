517a5c88f431b30b4f9ff7725f491990ecf0ceef
Teh Sunn Liu <tehsunnliu@gmail.com>
2022-05-23 12:25:13 +0530

    Moved file chunking outside enclave

--- enclave/src/lib.rs
+++ enclave/src/lib.rs
@@ -50,6 +50,43 @@ struct Signatures(Vec<G1>, PublicKey);
 
 static mut SIGNATURES: Signatures = Signatures(vec![], PublicKey::new(G2::zero()));
 
+struct Keys {
+    skey: SecretKey,
+    pkey: PublicKey,
+    sig: G1,
+    generated: bool,
+}
+
+impl Keys {
+    pub const fn new() -> Keys {
+        Keys {
+            skey: SecretKey::zero(),
+            pkey: PublicKey::zero(),
+            sig: G1::zero(),
+            generated: false,
+        }
+    }
+
+    pub fn gen_keys(self: &mut Self, seed: &[u8]) {
+        if !self.generated {
+            let (skey, pkey, sig) = pbc::key_gen_deterministic(seed);
+            self.skey = skey;
+            self.pkey = pkey;
+            self.sig = sig;
+            unsafe {
+                SIGNATURES.1 = pkey;
+            }
+            self.generated = true;
+        }
+    }
+
+    pub fn get_keys(self: &Self) -> (SecretKey, PublicKey, G1) {
+        (self.skey, self.pkey, self.sig)
+    }
+}
+
+static mut KEYS: Keys = Keys::new();
+
 #[no_mangle]
 pub extern "C" fn get_rng(length: usize, value: *mut u8) -> sgx_status_t {
     let mut random_vec = vec![0u8; length];
@@ -69,9 +106,19 @@ pub extern "C" fn get_rng(length: usize, value: *mut u8) -> sgx_status_t {
     sgx_status_t::SGX_SUCCESS
 }
 
+#[no_mangle]
+pub extern "C" fn gen_keys(seed: *const u8, seed_len: usize) -> sgx_status_t {
+    let s = unsafe { slice::from_raw_parts(seed, seed_len) };
+
+    pbc::init_pairings();
+    unsafe {
+        KEYS.gen_keys(s);
+    }
+
+    sgx_status_t::SGX_SUCCESS
+}
+
 /// Arguments:
-/// `seed` is used to compute keys
-/// `seed_len` is the number of **elements**, not the number of bytes.
 /// `data` is the data that needs to be processed. It should not exceed SGX max memory size
 /// `data_len` argument is the number of **elements**, not the number of bytes.
 /// `block_size` is the size of the chunks that the data will be sliced to, in bytes.
@@ -79,64 +126,62 @@ pub extern "C" fn get_rng(length: usize, value: *mut u8) -> sgx_status_t {
 /// memory to call `get_signatures`
 #[no_mangle]
 pub extern "C" fn process_data(
-    seed: *const u8,
-    seed_len: usize,
     data: *mut u8,
     data_len: usize,
-    block_size: usize,
-    sig_len: &mut usize,
-    multi_thread: bool,
+    sig_len: usize,
+    sig: *mut u8,
 ) -> sgx_status_t {
     let d = unsafe { slice::from_raw_parts(data, data_len).to_vec() };
-    let s = unsafe { slice::from_raw_parts(seed, seed_len) };
 
-    pbc::init_pairings();
-    let (skey, pkey, _sig) = pbc::key_gen_deterministic(s);
+    let (skey, pkey, _sig) = unsafe { KEYS.get_keys() };
 
-    let mut chunks: Vec<Vec<u8>> = Vec::new();
-    d.chunks(block_size).for_each(|chunk| {
-        chunks.push(chunk.to_vec());
-    });
-
-    let now = time::SystemTime::now();
-    let mut handles = vec![];
-    let mut signatures = Arc::new(SgxMutex::new(vec![G1::zero(); chunks.len()]));
-    if multi_thread {
-        println!("Multi-thread");
-
-        for i in 0..chunks.len() {
-            let chunk = chunks[i].to_vec().clone();
-            let skey = skey.clone();
-            let signatures = Arc::clone(&signatures);
-            let handle = thread::spawn(move || {
-                let sig = cess_bncurve::sign_message(&chunk, &skey);
-                let mut signature = signatures.lock().unwrap();
-                *signature.index_mut(i) = sig;
-            });
-            handles.push(handle);
-        }
-
-        for handle in handles {
-            handle.join().unwrap();
-        }
-    } else {
-        println!("Single-thread");
-
-        for i in 0..chunks.len() {
-            let chunk = chunks[i].to_vec().clone();
-            let sig = cess_bncurve::sign_message(&chunk, &skey);
-            let mut signature = signatures.lock().unwrap();
-            *signature.index_mut(i) = sig;
-        }
-    }
-    let escaped = now.elapsed().unwrap();
-    println!("Sig took {:.2?}", escaped);
-
-    *sig_len = chunks.len();
-
+    let signature = cess_bncurve::sign_message(&d, &skey);
     unsafe {
-        SIGNATURES = Signatures(signatures.lock().unwrap().to_vec(), pkey);
+        ptr::copy_nonoverlapping(signature.base_vector().as_ptr(), sig, sig_len);
     }
+
+    // let mut chunks: Vec<Vec<u8>> = Vec::new();
+    // d.chunks(block_size).for_each(|chunk| {
+    //     chunks.push(chunk.to_vec());
+    // });
+
+    // let now = time::SystemTime::now();
+    // let mut handles = vec![];
+    // let mut signatures = &mut vec![G1::zero(); chunks.len()];
+    // if multi_thread {
+    //     println!("Multi-thread");
+
+    //     for i in 0..chunks.len() {
+    //         let chunk = chunks[i].to_vec().clone();
+    //         let skey = skey.clone();
+    //         let mut signatures = signatures.clone();
+    //         let handle = thread::spawn(move || {
+    //             let sig = cess_bncurve::sign_message(&chunk, &skey);
+    //             signatures[i] = sig;
+    //         });
+    //         handles.push(handle);
+    //     }
+
+    //     for handle in handles {
+    //         handle.join().unwrap();
+    //     }
+    // } else {
+    //     println!("Single-thread");
+
+    //     for i in 0..chunks.len() {
+    //         let chunk = chunks[i].to_vec().clone();
+    //         let sig = cess_bncurve::sign_message(&chunk, &skey);
+    //         signatures[i] = sig;
+    //     }
+    // }
+    // let escaped = now.elapsed().unwrap();
+    // println!("Sig took {:.2?}", escaped);
+
+    // *sig_len = chunks.len();
+
+    // unsafe {
+    //     SIGNATURES = Signatures(signatures.to_vec(), pkey);
+    // }
 
     sgx_status_t::SGX_SUCCESS
 }
