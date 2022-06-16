de8a378a43e26ec9cb950b668cd97902c60fc192
jiangyizhe <1239865849@qq.com>
2022-06-16 09:50:45 +0800

    test

--- enclave/src/pbc.rs
+++ enclave/src/pbc.rs
@@ -75,12 +75,12 @@ pub fn init_pairings() {
         let len = cess_pbc::set_g1(context, g1.as_ptr() as *mut _);
         // returns nbr bytes read, should equal length of G1
         assert_eq!(len, BN_CURVE_INFO.g1_size as i64);
-        println!("g2 = {:?}", get_g1().base_vector());
 
         let mut g2 = vec![0u8; BN_CURVE_INFO.g2_size];
         hexstr_to_u8v(&(*BN_CURVE_INFO.g2), &mut g2);
         let len = cess_pbc::set_g2(context, g2.as_ptr() as *mut _);
         println!("g2 = {:?}",get_g2().to_str().into_bytes());
+        println!("g2' = {:?}",get_g2().base_vector());
         // returns nbr bytes read, should equal length of G2
         assert_eq!(len, BN_CURVE_INFO.g2_size as i64);
     }
