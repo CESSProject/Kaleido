ac551fd4cbbacac0c9d96ef7054fdef4d14b6ca7
jiangyizhe <1239865849@qq.com>
2022-06-28 15:08:52 +0800

    feat:add klib

---
+++ klib/build.rs
@@ -0,0 +1,12 @@
+extern crate cbindgen;
+use std::env;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR")
+        .expect("CARGO_MANIFEST_DIR env var is not defined");
+    let config = cbindgen::Config::from_file("cbindgen.toml")
+        .expect("Unable to find cbindgen.toml configuration file");
+    cbindgen::generate_with_config(&crate_dir, config)
+        .unwrap()
+        .write_to_file("./kaleido.h");
+} 
