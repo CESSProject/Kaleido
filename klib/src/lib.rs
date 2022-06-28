355c00b933d653c31cfe7e278c3b4c4a087bebd4
jiangyizhe <1239865849@qq.com>
2022-06-28 17:52:56 +0800

    test

--- klib/src/lib.rs
+++ klib/src/lib.rs
@@ -94,24 +94,24 @@ pub fn raw_ptr<T>(thing: T) -> *mut T {
     Box::into_raw(Box::new(thing))
 }
 
-fn init_enclave() -> SgxResult<SgxEnclave> {
-    let mut launch_token: sgx_launch_token_t = [0; 1024];
-    let mut launch_token_updated: i32 = 0;
-    // call sgx_create_enclave to initialize an enclave instance
-    // Debug Support: set 2nd parameter to 1
-    let debug = 1;
-    let mut misc_attr = sgx_misc_attribute_t {
-        secs_attr: sgx_attributes_t { flags: 0, xfrm: 0 },
-        misc_select: 0,
-    };
-    SgxEnclave::create(
-        ENCLAVE_FILE,
-        debug,
-        &mut launch_token,
-        &mut launch_token_updated,
-        &mut misc_attr,
-    )
-}
+// fn init_enclave() -> SgxResult<SgxEnclave> {
+//     let mut launch_token: sgx_launch_token_t = [0; 1024];
+//     let mut launch_token_updated: i32 = 0;
+//     // call sgx_create_enclave to initialize an enclave instance
+//     // Debug Support: set 2nd parameter to 1
+//     let debug = 1;
+//     let mut misc_attr = sgx_misc_attribute_t {
+//         secs_attr: sgx_attributes_t { flags: 0, xfrm: 0 },
+//         misc_select: 0,
+//     };
+//     SgxEnclave::create(
+//         ENCLAVE_FILE,
+//         debug,
+//         &mut launch_token,
+//         &mut launch_token_updated,
+//         &mut misc_attr,
+//     )
+// }
 
 #[no_mangle]
 pub extern "C" fn hello(name: *const libc::c_char) {
