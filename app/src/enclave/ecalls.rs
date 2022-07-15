fd9f0d77f2531c1929122fb31b8c41f1d7b8e60a
Demos Chiang <69138672+democ98@users.noreply.github.com>
2022-07-15 16:51:01 +0800

    feat:add remoteattestation code

--- app/src/enclave/ecalls.rs
+++ app/src/enclave/ecalls.rs
@@ -23,4 +23,34 @@ extern "C" {
         segment_size: usize,
         callback_url: *const c_char,
     ) -> sgx_status_t;
+    pub fn enclave_init_ra(
+        eid: sgx_enclave_id_t,
+        retval: *mut sgx_status_t,
+        b_pse:bool,
+        p_context: *mut sgx_ra_context_t,
+    ) -> sgx_status_t;
+    pub fn enclave_ra_close(
+        eid: sgx_enclave_id_t,
+        retval: *mut sgx_status_t,
+        context:sgx_ra_context_t,
+    ) -> sgx_status_t;
+    pub fn verify_att_result_mac(
+        eid: sgx_enclave_id_t,
+        retval: *mut sgx_status_t,
+        context:sgx_ra_context_t,
+        message: *mut u8,
+        message_size:usize,
+        mac: *mut u8,
+        mac_size:usize,
+    ) -> sgx_status_t;
+    pub fn verify_secret_data(
+        eid: sgx_enclave_id_t,
+        retval: *mut sgx_status_t,
+        context:sgx_ra_context_t,
+        p_secret: *mut u8,
+        secret_size:u32,
+        gcm_mac: *mut u8,
+        max_verification_length:u32,
+        p_ret: *mut u8,
+    ) -> sgx_status_t;
 }
