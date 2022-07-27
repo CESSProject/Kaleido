999c384b07be531c5383fc6e3575b5499056bd50
jiangyizhe <1239865849@qq.com>
2022-07-27 10:05:40 +0800

    test

--- enclave/src/secret_exchange/mod.rs
+++ enclave/src/secret_exchange/mod.rs
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License..
 
-// #![crate_name = "mra"]
+
 #![crate_type = "staticlib"]
 
 #![cfg_attr(not(target_env = "sgx"), no_std)]
@@ -27,7 +27,6 @@ pub mod hex;
 use std::backtrace::{self, PrintFormat};
 use sgx_types::*;
 use sgx_tse::*;
-//use sgx_trts::trts::{rsgx_raw_is_outside_enclave, rsgx_lfence};
 use sgx_tcrypto::*;
 use sgx_rand::*;
 
@@ -49,29 +48,7 @@ pub const SIGRL_SUFFIX:&'static str = "/sgx/dev/attestation/v4/sigrl/";
 pub const REPORT_SUFFIX:&'static str = "/sgx/dev/attestation/v4/report";
 pub const CERTEXPIRYDAYS: i64 = 90i64;
 
-// extern "C" {
-//     pub fn ocall_sgx_init_quote ( ret_val : *mut sgx_status_t,
-//                                   ret_ti  : *mut sgx_target_info_t,
-//                                   ret_gid : *mut sgx_epid_group_id_t) -> sgx_status_t;
-//     pub fn ocall_get_ias_socket ( ret_val : *mut sgx_status_t,
-//                                   ret_fd  : *mut i32) -> sgx_status_t;
-//     pub fn ocall_get_quote (ret_val            : *mut sgx_status_t,
-//                             p_sigrl            : *const u8,
-//                             sigrl_len          : u32,
-//                             p_report           : *const sgx_report_t,
-//                             quote_type         : sgx_quote_sign_type_t,
-//                             p_spid             : *const sgx_spid_t,
-//                             p_nonce            : *const sgx_quote_nonce_t,
-//                             p_qe_report        : *mut sgx_report_t,
-//                             p_quote            : *mut u8,
-//                             maxlen             : u32,
-//                             p_quote_len        : *mut u32) -> sgx_status_t;
-//     #[allow(dead_code)]
-//     pub fn ocall_get_update_info (ret_val: *mut sgx_status_t,
-//                                   platformBlob: * const sgx_platform_info_t,
-//                                   enclaveTrusted: i32,
-//                                   update_info: * mut sgx_update_info_bit_t) -> sgx_status_t;
-// }
+
 
 
 fn parse_response_attn_report(resp : &[u8]) -> (String, String, String){
