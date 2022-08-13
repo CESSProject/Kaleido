b3c4f4d08271dee9b59c571e1e4fd5af9437127b
jiangyizhe <1239865849@qq.com>
2022-08-13 14:05:10 +0800

    test

--- enclave/src/podr2_proof_commit.rs
+++ enclave/src/podr2_proof_commit.rs
@@ -31,7 +31,7 @@ pub fn podr2_proof_commit(
     //Add zeros after the excess file
     let mut extra_len =data.len() as isize %block_size as isize;
     if extra_len>0{
-        let zero_pad_len=block_size as isize - data.len() as isize;
+        let zero_pad_len=block_size as isize - extra_len;
         info!("zero_pad_len:{},data length {}",zero_pad_len,data.len());
         data.append(&mut vec![0u8; zero_pad_len as usize]);
         info!("data length after append1:{}",data.len());
