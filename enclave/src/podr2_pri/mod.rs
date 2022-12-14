d95c347bdfa2579cb8da333391bc749c89db9a81
Alex (Teh Sunn Liu) <tehsunnliu@gmail.com>
2022-12-14 17:57:51 +0530

    Added ProofTimer to chal_gen (#83)

--- enclave/src/podr2_pri/mod.rs
+++ enclave/src/podr2_pri/mod.rs
@@ -1,12 +1,45 @@
+use std::sync::SgxMutex;
+
 use alloc::string::{String, ToString};
 use alloc::vec::Vec;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-pub mod key_gen;
-pub mod sig_gen;
 pub mod chal_gen;
 pub mod gen_proof;
+pub mod key_gen;
+pub mod sig_gen;
 pub mod verify_proof;
+
+#[derive(Serializable, DeSerializable)]
+pub struct ProofTimer {
+    /// Unique random value sent by CESS Chain
+    pub id: Vec<u8>,
+    /// Epoch Time sent by CESS Chain in seconds
+    pub time: u64,
+}
+
+impl PartialEq for ProofTimer {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id && self.time == other.time
+    }
+}
+
+#[derive(Serializable, DeSerializable)]
+struct ProofTimerList {
+    timers: Vec<ProofTimer>,
+}
+
+impl ProofTimerList {
+    pub const fn new() -> ProofTimerList {
+        ProofTimerList {
+            timers: Vec::new(),
+        }
+    }
+}
+
+lazy_static! (
+    static ref PROOF_TIMER_LIST: SgxMutex<ProofTimerList> = SgxMutex::new(ProofTimerList::new());
+);
 
 #[derive(Clone)]
 pub struct Tag {
@@ -16,6 +49,7 @@ pub struct Tag {
 
 impl Tag {
     pub fn new() -> Tag {
+        use std::time::SystemTime;
         Tag {
             t: Tag0 { n: 0, enc: vec![] },
             mac_t0: vec![],
@@ -33,10 +67,7 @@ pub struct Tag0 {
 
 impl Tag0 {
     pub fn new() -> Tag0 {
-        Tag0 {
-            n: 0,
-            enc: vec![],
-        }
+        Tag0 { n: 0, enc: vec![] }
     }
 }
 
@@ -51,12 +82,12 @@ impl EncEncrypt {
     pub fn new() -> EncEncrypt {
         EncEncrypt {
             prf: "".to_string(),
-            alpha: vec![]
+            alpha: vec![],
         }
     }
 }
 #[derive(Clone)]
 pub struct QElement {
     pub i: i64,
-    pub v: i64
+    pub v: i64,
 }
